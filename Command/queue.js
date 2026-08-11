a43ca724a7c92ba022d806ff29673ce02897f14f
g3xmohamedx11-blip <g3xmohamedx11@gmail.com>
2026-08-11 21:11:10 +0300

    Update queue.js

--- Command/queue.js
+++ Command/queue.js
@@ -1,10 +1,17 @@
-
 module.exports = {
   name: "queue",
   run: async (client, interaction) => {
-    const queue = client.distube.getQueue(interaction);
-    if (!queue) return interaction.reply("Empty queue");
 
-    interaction.reply(queue.songs.map((s,i)=>`${i+1}. ${s.name}`).join("\n"));
+    const queue = client.distube.getQueue(interaction.guildId);
+    if (!queue) return interaction.reply(client.lang.QUEUE_EMPTY);
+
+    const embed = {
+      color: 0xff3c00,
+      title: client.lang.QUEUE_TITLE,
+      description: queue.songs.map((s, i) => `**${i + 1}.** ${s.name}`).join("\n")
+    };
+
+    interaction.reply({ embeds: [embed] });
+
   }
 };
